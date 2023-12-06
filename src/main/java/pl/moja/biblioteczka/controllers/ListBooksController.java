a8607aac01190e9c5a7af7cb65118ea1d5f9b239
Arleta Madej <arleta.madej@gmail.com>
2023-12-06 13:53:38 +0100

    added ListBooks.fxml

--- src/main/java/pl/moja/biblioteczka/controllers/ListBooksController.java
+++ src/main/java/pl/moja/biblioteczka/controllers/ListBooksController.java
@@ -1,4 +1,11 @@
 package pl.moja.biblioteczka.controllers;
 
+import javafx.fxml.FXML;
+
 public class ListBooksController {
+
+    @FXML
+    void initialize(){
+        System.out.println("Hello");
+    }
 }
